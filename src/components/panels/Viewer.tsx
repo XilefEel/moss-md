2bf04edfa4298d10d5d12bb4eabca39257f9a184
Felix Lee <felixlee175@gmail.com>
2026-05-24 22:17:16 +0700

    added custom vitepress-like containers

--- src/components/panels/Viewer.tsx
+++ src/components/panels/Viewer.tsx
@@ -1,6 +1,55 @@
 import MarkdownIt from "markdown-it";
+import container from "markdown-it-container";
 
 const md = new MarkdownIt();
+
+md.use(container, "tip", {
+  render(tokens: any[], idx: number) {
+    const token = tokens[idx];
+    const title = token.info.trim().slice("tip".length).trim();
+    if (token.nesting === 1) {
+      return `<div class="tip"><p class="custom-block-title">${title || "TIP"}</p>\n`;
+    }
+
+    return "</div>\n";
+  },
+});
+
+md.use(container, "info", {
+  render(tokens: any[], idx: number) {
+    const token = tokens[idx];
+    const title = token.info.trim().slice("info".length).trim();
+    if (token.nesting === 1) {
+      return `<div class="info"><p class="custom-block-title">${title || "INFO"}</p>\n`;
+    }
+
+    return "</div>\n";
+  },
+});
+
+md.use(container, "warning", {
+  render(tokens: any[], idx: number) {
+    const token = tokens[idx];
+    const title = token.info.trim().slice("warning".length).trim();
+    if (token.nesting === 1) {
+      return `<div class="warning"><p class="custom-block-title">${title || "WARNING"}</p>\n`;
+    }
+
+    return "</div>\n";
+  },
+});
+
+md.use(container, "danger", {
+  render(tokens: any[], idx: number) {
+    const token = tokens[idx];
+    const title = token.info.trim().slice("danger".length).trim();
+    if (token.nesting === 1) {
+      return `<div class="danger"><p class="custom-block-title">${title || "DANGER"}</p>\n`;
+    }
+
+    return "</div>\n";
+  },
+});
 
 export default function Viewer({ content }: { content: string }) {
   return (
